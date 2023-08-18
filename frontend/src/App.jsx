469321f52a601fdbf07d706d8c9d326ff4854959
Gessmass <martinweilldrums@gmail.com>
2023-08-18 23:16:10 +0200

    routes ok

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,24 @@
-import Home from "./pages/Home"
-
-import "./App.css"
+import { Routes, Route } from "react-router-dom"
+import Home from "./Pages/Home"
+import Contact from "./Pages/Contact"
+import About from "./Pages/About"
+import QuizzSelon from "./Pages/QuizzSelon"
+import QuizzCultureT from "./Pages/QuizzCultureT"
+import Categories from "./Pages/Categories"
+import "./App.scss"
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <p>coucou</p>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/QuizzSelon" element={<QuizzSelon />} />
+        <Route path="/QuizzCultureT" element={<QuizzCultureT />} />
+        <Route path="/Categories" element={<Categories />} />
+      </Routes>
+    </>
   )
 }
 
