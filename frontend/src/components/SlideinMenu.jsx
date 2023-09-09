05e8b8f06b49a3b08dbc3da7b4dc02dcbba8d4f8
Gessmass <martinweilldrums@gmail.com>
2023-09-10 00:11:57 +0200

    toujours sur le bouton burger menu slide in

--- frontend/src/components/SlideinMenu.jsx
+++ frontend/src/components/SlideinMenu.jsx
@@ -1,22 +1,5 @@
-import { Link } from "react-router-dom"
+// import { Link } from "react-router-dom"
 
 export default function SlideinMenu() {
-  return (
-    <div id="slideinMenuDiv">
-      <ul>
-        <li>
-          <Link to="/">Accueil</Link>
-        </li>
-        <li>
-          <Link to="/Categories">Categories</Link>
-        </li>
-        <li>
-          <Link to="/Contact">Contact</Link>
-        </li>
-        <li>
-          <Link to="/About">À Propos</Link>
-        </li>
-      </ul>
-    </div>
-  )
+  return <></>
 }
