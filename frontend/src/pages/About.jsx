dfa31eee81e6985da19ca719a2910a314e022555
Nestadar <Nestadar@gmail.com>
2023-09-09 23:11:41 +0200

    About page is finished

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,11 +1,65 @@
 import Navbar from "../components/Navbar"
-
+import Footer from "../components/Footer"
 const About = () => {
   return (
-    <>
+    <div id="abousUsMainDiv">
       <Navbar />
-      <h1>Je suis dans About</h1>
-    </>
+      <div id="topDivAboutUs">
+        <h1>Qui sommes nous ?</h1>
+      </div>
+      <div id="mainContentContainer">
+        <div id="mainContent">
+          <p>
+            Bienvenue sur notre plateforme dédiée à l'émission captivante et
+            éclairante "Thinkerview" ! Permettez-nous de vous présenter
+            l'histoire et les aspirations qui ont donné naissance à ce projet.
+          </p>
+          <p>
+            Notre aventure a débuté lors de nos années d'études en développement
+            web, un moment où notre passion pour la technologie et le contenu
+            intellectuellement stimulant s'est conjuguée pour créer quelque
+            chose de spécial. Il est important de noter que ce site n'est en
+            aucun cas un produit officiel de "Thinkerview". Cependant, nous
+            sommes fiers de vous informer que nous avons obtenu l'aimable
+            autorisation de "Thinkerview" pour mettre en place cette plateforme.
+          </p>
+          <p>
+            Notre admiration pour "Thinkerview" est palpable. Cette émission,
+            qui offre une tribune unique à des penseurs et experts de divers
+            horizons, nous a profondément marqués. En partageant nos sentiments
+            et notre amour pour cette émission, nous souhaitons transmettre
+            l'essence même de ce qui nous inspire. Notre curiosité insatiable et
+            notre esprit critique aiguisé nous permettent de garder une
+            perspective équilibrée, où chaque opinion trouve sa place.
+          </p>
+          <p>
+            Dans un monde où l'apprentissage et le divertissement ne sont pas
+            mutuellement exclusifs, nous aspirons à créer une expérience
+            interactive pour nos visiteurs. Notre objectif est de vous offrir un
+            outil à la fois ludique et éducatif. En évaluant vos connaissances à
+            travers des quiz pertinents après chaque visionnage, nous souhaitons
+            renforcer votre compréhension tout en ajoutant une dose de plaisir à
+            votre expérience.
+          </p>
+          <p>
+            Mais notre mission ne s'arrête pas là. Nous avons également à cœur
+            d'étendre les frontières de la connaissance en faisant découvrir
+            "Thinkerview" à ceux qui ne l'ont pas encore rencontrée. Pour les
+            connaisseurs de l'émission, nous souhaitons approfondir davantage
+            leur compréhension en explorant ses aspects les plus subtils et
+            nuancés.
+          </p>
+          <p>
+            Nous vous invitons à vous joindre à nous dans cette exploration
+            fascinante du monde des idées et du savoir. Ensemble, nous pouvons
+            faire en sorte que l'apprentissage soit non seulement informatif,
+            mais aussi divertissant et inspirant. Merci de nous rejoindre dans
+            cette aventure passionnante avec "Thinkerview" !
+          </p>
+        </div>
+      </div>
+      <Footer />
+    </div>
   )
 }
 
