a1e79e791a184dfb5439954204433114100dfbb0
Nestadar <Nestadar@gmail.com>
2023-09-25 16:39:42 +0200

    oauiif oauf

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,33 +1,26 @@
-import Navbar from "../components/Navbar"
-import Footer from "../components/Footer"
-
 const Register = () => {
   return (
-    <div id="mainDivRegister">
-      <Navbar />
-      <div id="mainContentRegister">
-        <div id="titleRegister">
-          <span>Rejoindre la communauté ThinkerViewed</span>
-        </div>
-        <div id="firstInputDivRegister">
-          <input className="inputRegister" placeholder="Adresse e-mail*" />
-          <input className="inputRegister" placeholder="Pseudo*" />
-        </div>
-        <div id="lastInputRegister">
-          <input className="inputRegister" placeholder="Mot de Passe*" />
-          <input
-            className="inputRegister"
-            placeholder="Confirmez votre mot de passe*"
-          />
-        </div>
-        <div id="haveToMention">
-          <span>Les champs contenant * sont obligatoires</span>
-        </div>
-        <div id="buttonRegister">
-          <button type="button">Inscription</button>
-        </div>
+    <div id="mainContentRegister">
+      <div id="titleRegister">
+        <span>Rejoindre la communauté ThinkerViewed</span>
       </div>
-      <Footer />
+      <div id="firstInputDivRegister">
+        <input className="inputRegister" placeholder="Adresse e-mail*" />
+        <input className="inputRegister" placeholder="Pseudo*" />
+      </div>
+      <div id="lastInputRegister">
+        <input className="inputRegister" placeholder="Mot de Passe*" />
+        <input
+          className="inputRegister"
+          placeholder="Confirmez votre mot de passe*"
+        />
+      </div>
+      <div id="haveToMention">
+        <span>Les champs contenant * sont obligatoires</span>
+      </div>
+      <div id="buttonRegister">
+        <button type="button">Inscription</button>
+      </div>
     </div>
   )
 }
