323026392e745802beb7a0e0725a7fb837418c6e
Nestadar <Nestadar@gmail.com>
2023-09-27 18:43:17 +0200

    prunes et loutres

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,9 +1,17 @@
-// import { useState, useEffect } from "react"
+import { useState } from "react"
 // import axios from "axios"
+// , useEffect
+
+import Bronze from "../assets/images/Bronze.png"
+import Chocolate from "../assets/images/Chocolate.png"
+import Diamond from "../assets/images/Diamond.png"
+import Iron from "../assets/images/Iron.png"
+import Silver from "../assets/images/Silver.png"
+import Gold from "../assets/images/Gold.png"
 
 const Profile = () => {
   //   const [user, setUser] = useState([])
-  //   const [switchInput, setSwitchInput] = useState(false)
+  const [isModifying, setIsModifying] = useState(false)
 
   return (
     <div id="mainDivProfile">
@@ -29,16 +37,57 @@ const Profile = () => {
           <span>MEDAILLES</span>
         </div>
         <div id="medalsRackDiv">
-          picture 1 picture 2 picture 3 picture 4 picture 5 picture 6
+          <div id="medalsImagesContainer">
+            <img src={Diamond} alt="Medaille Diamant" />
+            <img src={Gold} alt="Medaille d'or" />
+            <img src={Silver} alt="Medaille d'argent" />
+            <img src={Bronze} alt="Medaille de Bronze" />
+            <img src={Iron} alt="Medaille en fer" />
+            <img src={Chocolate} alt="Medaille en chocolat" />
+          </div>
+          <div id="medalsNumberContainer">
+            <div className="medalsNumberDisplay">
+              <span>x1</span>
+            </div>
+            <div className="medalsNumberDisplay">
+              <span>x12</span>
+            </div>
+            <div className="medalsNumberDisplay">
+              <span>x11</span>
+            </div>
+            <div className="medalsNumberDisplay">
+              <span>x15</span>
+            </div>
+            <div className="medalsNumberDisplay">
+              <span>x12</span>
+            </div>
+            <div className="medalsNumberDisplay">
+              <span>x14</span>
+            </div>
+          </div>
         </div>
       </div>
       <div id="lastContentDiv">
+        <div id="titleLastDivProfile">
+          <span>INFOS COMPTE</span>
+        </div>
         <div id="inputDisplayInfo">
-          <span>Pseudo : userPseudo</span>
-          <span>E-mail : userEmail</span>
+          {isModifying === true ? (
+            <>
+              <input type="text" placeholder="Pseudo" />
+              <input type="email" placeholder="E-mail" />
+            </>
+          ) : (
+            <>
+              <span>Pseudo : userPseudo</span>
+              <span>E-mail : userEmail</span>
+            </>
+          )}
         </div>
         <div id="buttonModifyProfileDiv">
-          <button type="button">Modifier</button>
+          <button type="button" onClick={() => setIsModifying(!isModifying)}>
+            {isModifying === false ? "Modifier" : "Valider"}
+          </button>
         </div>
         <div id="newPasswordProfileDiv">
           <button type="button">Nouveau mot de passe</button>
